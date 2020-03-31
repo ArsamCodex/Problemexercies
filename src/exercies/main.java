f12304e571e40199684eda77567399968d792f4d
ArsamCodex <62879887+ArsamCodex@users.noreply.github.com>
2020-03-31 23:20:28 +0200

    Update main.java

--- src/exercies/main.java
+++ src/exercies/main.java
@@ -2,7 +2,6 @@ package exercies;
 
 public class main {
     public static void main ( String [] args){
-        Account ex = new Account("Armin" , "JANUARI" , 3);
-        ex.isSameMonth();
+       
     }
 }
