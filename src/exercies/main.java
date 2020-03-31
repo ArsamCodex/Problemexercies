8f71cbb89aa6c8b99bed594bf90abf67b17930c4
ArsamCodex <62879887+ArsamCodex@users.noreply.github.com>
2020-03-31 23:34:07 +0200

    Update main.java

--- src/exercies/main.java
+++ src/exercies/main.java
@@ -2,6 +2,12 @@ package exercies;
 
 public class main {
     public static void main ( String [] args){
-       
+       Account a1 = new Account();
+        a1.display();
+        a1.checkBalance();
+        a1.verzenden(1000);
+        a1.checkBalance();a1.withdraw(1500);
+        a1.checkBalance();
+        
     }
 }
