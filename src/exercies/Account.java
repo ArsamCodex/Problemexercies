206f4a251c899aa545f5aad18c5b2328ad197f51
arsamparsa777 <62879887+ArsamCodex@users.noreply.github.com>
2020-03-31 23:17:16 +0200

    solve problem question

---
+++ src/exercies/Account.java
@@ -0,0 +1,6 @@
+package exercies;
+
+public class Account {
+       
+
+}
---
+++ src/exercies/main.java
@@ -0,0 +1,8 @@
+package exercies;
+
+public class main {
+    public static void main ( String [] args){
+        Account ex = new Account("Armin" , "JANUARI" , 3);
+        ex.isSameMonth();
+    }
+}
