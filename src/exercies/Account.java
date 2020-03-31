50e968a60c6e165be0a2161cd4c4dcb8163c3f37
ArsamCodex <62879887+ArsamCodex@users.noreply.github.com>
2020-03-31 23:31:59 +0200

    I

--- src/exercies/Account.java
+++ src/exercies/Account.java
@@ -1,6 +1,37 @@
 package exercies;
 
-public class Account {
+ class Account {
+        int RekeningNummer;
+        String Naam;
+        float Balance;
+        
+  public void Invullen(int r , String n, float d){
+         Rekeningnummer = r;
+         naam = n;
+         Balace = d;
+
+  }
+  
+   public void Deposite (float d){
+          Balance =   d + Balance;
+          sout(d + " AKKORD "); 
+          
+   }
+        
+   PUBLIC void withdraw(float d){
+          if( Balance < d ) {
+                 sout(" ONVOLDOENDE"" );
+                      } else {
+                             Balance = Balance - d;
+                             sout(" whirhdraww " + d);
+                      }
+                      }
+   public void checkBalance (){
+       sout("balance is "  + d);     
+   }
+                      
+    public void display()
+                      {sout(RekeningNummer + " " + Naam + " "+ Balance);
        
 
 }
